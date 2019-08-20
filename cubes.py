d9801a49344c3a3d8a44dce68ca2ede65573c607
lubiedak <l.biedak@wp.pl>
2019-08-20 21:41:20 +0200

    Remove trivials

--- cubes.py
+++ cubes.py
@@ -1,11 +1,12 @@
 n = 100
 results=set()
 separator=', '
+trivial=[1,8,27,64]
 for i in range(-n,n):
   for j in range(-n,n):
     for k in range(-n,n):
       val = i*i*i + j*j*j + k*k*k 
-      if val!=1 and val < n and val > 0:
+      if val not in trivial and val < n and val > 0:
         factors = sorted([i,j,k])
         factorsStr = [str(x) for x in factors]
         results.add(str(val) + "=" + separator.join(factorsStr))
